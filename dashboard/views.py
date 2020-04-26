415b2c268554093f14c3a93ba4f18e81aa77f9c8
rahul292-cloud <imrsy292@gmail.com>
2020-04-26 12:56:26 +0530

    [chg]

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,42 +11,44 @@ import openpyxl
 def register(request):
     return render(request, 'dashboard/register.html')
 
-def vehicle(request):
-    if "GET" == request.method:
-        return render(request, 'dashboard/vehicleDetails.html', {})
-    else:
-        excel_file = request.FILES["excel_file"]
+# def vehicle(request):
+#     if "GET" == request.method:
+#         return render(request, 'dashboard/vehicleDetails.html', {})
+#     else:
+#         excel_file = request.FILES["excel_file"]
+#
+#         # you may put validations here to check extension or file size
+#
+#         wb = openpyxl.load_workbook(excel_file)
+#
+#         # getting all sheets
+#         sheets = wb.sheetnames
+#         print(sheets)
+#
+#         # getting a particular sheet
+#         worksheet = wb["Sheet1"]
+#         print(worksheet)
+#
+#         # getting active sheet
+#         active_sheet = wb.active
+#         print(active_sheet)
+#
+#         # reading a cell
+#         print(worksheet["A1"].value)
+#
+#         excel_data = list()
+#         # iterating over the rows and
+#         # getting value from each cell in row
+#         for row in worksheet.iter_rows():
+#             row_data = list()
+#             for cell in row:
+#                 row_data.append(str(cell.value))
+#                 print(cell.value)
+#             excel_data.append(row_data)
+#
+#         return render(request, 'dashboard/vehicleDetails.html', {"excel_data": excel_data})
 
-        # you may put validations here to check extension or file size
 
-        wb = openpyxl.load_workbook(excel_file)
-
-        # getting all sheets
-        sheets = wb.sheetnames
-        print(sheets)
-
-        # getting a particular sheet
-        worksheet = wb["Sheet1"]
-        print(worksheet)
-
-        # getting active sheet
-        active_sheet = wb.active
-        print(active_sheet)
-
-        # reading a cell
-        print(worksheet["A1"].value)
-
-        excel_data = list()
-        # iterating over the rows and
-        # getting value from each cell in row
-        for row in worksheet.iter_rows():
-            row_data = list()
-            for cell in row:
-                row_data.append(str(cell.value))
-                print(cell.value)
-            excel_data.append(row_data)
-
-        return render(request, 'dashboard/vehicleDetails.html', {"excel_data": excel_data})
 
 
 def index(request):
